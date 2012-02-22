6bdc9df57ee60cc29c4125f4edf1c67e5ceda85e
Érico Andrei <erico@simplesconsultoria.com.br>
2012-02-22 12:16:19 -0200

    Add a very basic catalog multiplex support

--- src/collective/person/content/person.py
+++ src/collective/person/content/person.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+from Acquisition import aq_base
 from five import grok
 from zope import schema
 from zope.component import queryUtility
@@ -13,6 +14,8 @@ from plone.indexer import indexer
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 
 from plone.namedfile.field import NamedImage
+
+from Products.CMFCore.utils import getToolByName
 
 from collective.person.utils import check_birthday
 from collective.person.catalog import IPersonCatalog
@@ -116,6 +119,52 @@ class Person(dexterity.Item):
     """ A Person
     """
     grok.implements(IPerson)
+
+    # Catalog Multiplex support
+    def _catalogs(self):
+        ''' catalogs we will use '''
+        return [getToolByName(self, 'portal_catalog'),
+                getToolByName(self, 'portal_personcatalog'),
+               ]
+
+    def indexObject(self):
+        ''' index an object on all registered catalogs '''
+        for c in self._catalogs():
+            c.catalog_object(self)
+
+    def unindexObject(self):
+        ''' remove an object from all registered catalogs '''
+        for c in self._catalogs():
+            c.uncatalog_object(self)
+
+    def reindexObjectSecurity(self, skip_self=False):
+        ''' reindex only security information on catalogs '''
+        path = '/'.join(self.getPhysicalPath())
+        for c in self._catalogs():
+            for brain in c.unrestrictedSearchResults(path=path):
+                brain_path = brain.getPath()
+                if brain_path == path and skip_self:
+                    continue
+                # Get the object
+                ob = brain._unrestrictedGetObject()
+
+                # Recatalog with the same catalog uid.
+                # _cmf_security_indexes in CMFCatalogAware
+                c.reindexObject(ob,
+                                idxs=self._cmf_security_indexes,
+                                update_metadata=0,
+                                uid=brain_path)
+
+    def reindexObject(self, idxs=[]):
+        ''' reindex object '''
+        if idxs == []:
+            # Update the modification date.
+            if hasattr(aq_base(self), 'notifyModified'):
+                self.notifyModified()
+        for c in self._catalogs():
+            if c is not None:
+                c.reindexObject(self,
+                                idxs=idxs)
 
     def Title(self):
         ''' Return a title from given_name and surname '''
