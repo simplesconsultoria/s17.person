0dabdff80cb15500e822e9b66278bf66f32bb34c
hvelarde <hector.velarde@gmail.com>
2012-04-04 15:31:16 -0500

    cosmetic changes to package declaration

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+
 import os
 from setuptools import setup, find_packages
 
@@ -6,15 +7,14 @@ version = '1.0'
 
 setup(name='collective.person',
       version=version,
-      description="A package containing a Dexterity content type and behaviors\
-                   to represent a Person as a content in a Plone site",
+      description="A package containing a Dexterity content type and behaviors "
+                  "to represent a Person as a content in a Plone site",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.1",
         "Framework :: Zope2",
-        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
@@ -32,7 +32,6 @@ setup(name='collective.person',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
           'plone.behavior',
           'plone.app.dexterity>=1.1',
           'plone.app.referenceablebehavior',
@@ -44,8 +43,6 @@ setup(name='collective.person',
         'test': ['plone.app.testing'],
         },
       entry_points="""
-      # -*- Entry points: -*-
-
       [z3c.autoinclude.plugin]
       target = plone
       """,
