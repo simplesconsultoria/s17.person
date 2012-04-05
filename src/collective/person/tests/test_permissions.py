12ee489c446a7aeee8bacb786c11e7b5956285ad
hvelarde <hector.velarde@gmail.com>
2012-04-05 17:45:33 -0500

    added test for permission

---
+++ src/collective/person/tests/test_permissions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import unittest2 as unittest
+
+from collective.person.testing import INTEGRATION_TESTING
+
+
+class PermissionsTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_add_permissions(self):
+        permission = 'collective.person: Add person'
+        expected = ['Contributor', 'Manager', 'Owner', 'Site Administrator']
+        roles = self.portal.rolesOfPermission(permission)
+        roles = [r['name'] for r in roles if r['selected']]
+        self.assertListEqual(roles, expected)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
