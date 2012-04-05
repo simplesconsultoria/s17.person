1d27c338bcd8a4e6310926f30994e9df48af58b4
Érico Andrei <erico@simplesconsultoria.com.br>
2012-04-04 22:02:35 -0300

    Change the way we install datagridfield

--- src/collective/person/upgrades/to1000.py
+++ src/collective/person/upgrades/to1000.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
-import logging
-
-from zope import component
 
 from Products.CMFCore.utils import getToolByName
-from Products.GenericSetup import interfaces as gsinterfaces
-from Products.GenericSetup.upgrade import listUpgradeSteps
 
 PROJECT = 'collective.person'
 
@@ -13,5 +8,8 @@ PROJECT = 'collective.person'
 def fromZero(context):
     ''' Upgrade from Zero to version 1000
     '''
-    # Do something here
-    pass
+    qi = getToolByName(context, 'portal_quickinstaller')
+    qi.installProduct('collective.z3cform.datagridfield',
+                      locked=0,
+                      hidden=0,
+                      profile='collective.z3cform.datagridfield:default')
