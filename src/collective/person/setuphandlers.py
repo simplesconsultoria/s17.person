d088f288996f0b1d330c95694d3388e69a726467
Alejandro Pereira <alepereira86@gmail.com>
2012-05-09 19:57:21 -0300

    adding import steps and profile for demos

--- src/collective/person/setuphandlers.py
+++ src/collective/person/setuphandlers.py
@@ -30,3 +30,57 @@ def run_upgrades(context):
                                                           _PROFILE_ID)
             setup_tool.setLastVersionForProfile(_PROFILE_ID, oStep.dest)
             logger.info(msg)
+
+
+def demo_steps(context):
+    """ Run steps to prepare a demo.
+    """
+    if context.readDataFile('collective.person-demo.txt') is None:
+        return
+    portal = context.getSite()
+    portal.invokeFactory('Folder', 'Pessoas')
+    folder = portal['Empregados']
+    list_users = [{'name':'marcelo-santos', 'password':'pass1'},
+                  {'name':'rodrigo-alves', 'password':'pass2'},
+                  {'name':'daniela-Alvarez', 'password':'pass3'},
+                  {'name':'juan-perez', 'password':'pass4'},
+                  {'name':'gustavo-rodriguez', 'password':'pass5'},]
+
+    for user in list_users:
+        create_user(user['name'], user['password'], portal)
+
+    # Set behaviors to person
+    behaviors = ['collective.person.behaviors.user.IPloneUser',
+                 'collective.person.behaviors.contact.IContactInfo']
+    fti = queryUtility(IDexterityFTI,
+                        name='collective.person.person')
+    fti.behaviors = tuple(behaviors)
+
+    for user in list_users:
+        person = user['name']
+        fullname = person.split('-')
+        folder.invokeFactory('collective.person.person', person)
+        folder[person].given_name = fullname[0].capitalize()
+        folder[person].surname = fullname[1].capitalize()
+        p1_contact = IContactInfo(folder[person])
+        p1_contact.instant_messengers = [{'category': u'skype',
+                                          'data': u'%s' % person.replace('-','_')}]
+        p1_ploneuser = IPloneUser(folder[person])
+        p1_ploneuser.user_name = person
+        folder[person].reindexObject()
+
+    import transaction
+    transaction.commit()
+
+    logger = logging.getLogger(PROJECTNAME)
+    setup.run_upgrades_for_profile(PROFILE_ID, context, logger)
+
+
+def create_user(username, password, portal):
+    properties = {
+    'username': username,
+    'fullname': (u'%s' % username).encode("utf-8"),
+    'email': u'%s@email.com' % username,
+    }
+    reg_tool = getToolByName(portal, 'portal_registration')
+    reg_tool.addMember(username, password, properties=properties)
