941657513d6d61f74967630dc99887e54e339c49
hvelarde <hector.velarde@gmail.com>
2012-05-09 17:46:46 -0300

    updated package declaration

--- setup.py
+++ setup.py
@@ -4,18 +4,28 @@ import os
 from setuptools import setup, find_packages
 
 version = '1.0'
+description = "A package containing a Dexterity content type and behaviors \
+to represent a Person as a content in a Plone site."
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.person',
       version=version,
-      description="A package containing a Dexterity content type and behaviors "
-                  "to represent a Person as a content in a Plone site",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description=description,
+      long_description=long_description,
       classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.1",
-        "Framework :: Zope2",
+        "Framework :: Plone :: 4.2",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Office/Business",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
         "Topic :: Software Development :: Libraries :: Python Modules",
@@ -33,7 +43,7 @@ setup(name='collective.person',
       install_requires=[
           'setuptools',
           'plone.behavior',
-          'plone.app.dexterity>=1.1',
+          'plone.app.dexterity>=1.2.1',
           'plone.app.referenceablebehavior',
           'plone.namedfile[blobs]',
           'plone.formwidget.namedfile',
