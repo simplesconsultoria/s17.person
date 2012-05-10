62b59cd981c60d62f59b947cf289628f8b5db7ce
Alejandro Pereira <alepereira86@gmail.com>
2012-05-10 11:25:58 -0300

    adding import steps to create users and images for they users portraits

--- src/collective/person/setuphandlers.py
+++ src/collective/person/setuphandlers.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 import logging
 
+import os
+
+from datetime import datetime
+
+from zope.component import queryUtility
+
+from plone.namedfile import NamedImage
+from plone.namedfile.tests.base import getFile
+
+from plone.dexterity.interfaces import IDexterityFTI
+
 from Products.CMFCore.utils import getToolByName
 
 from Products.GenericSetup.upgrade import listUpgradeSteps
+
+from collective.person.behaviors.contact import IContactInfo
+from collective.person.behaviors.user import IPloneUser
 
 
 _PROJECT = 'collective.person'
@@ -39,12 +53,12 @@ def demo_steps(context):
         return
     portal = context.getSite()
     portal.invokeFactory('Folder', 'Pessoas')
-    folder = portal['Empregados']
-    list_users = [{'name':'marcelo-santos', 'password':'pass1'},
-                  {'name':'rodrigo-alves', 'password':'pass2'},
-                  {'name':'daniela-Alvarez', 'password':'pass3'},
-                  {'name':'juan-perez', 'password':'pass4'},
-                  {'name':'gustavo-rodriguez', 'password':'pass5'},]
+    folder = portal['Pessoas']
+    list_users = [{'name':'marcelo-santos', 'password':'pass1', 'number': '1'},
+                  {'name':'rodrigo-alves', 'password':'pass2', 'number': '2'},
+                  {'name':'julia-Alvarez', 'password':'pass3', 'number': '3'},
+                  {'name':'juan-perez', 'password':'pass4', 'number': '4'},
+                  {'name':'gustavo-roner', 'password':'pass5', 'number': '5'}]
 
     for user in list_users:
         create_user(user['name'], user['password'], portal)
@@ -59,21 +73,40 @@ def demo_steps(context):
     for user in list_users:
         person = user['name']
         fullname = person.split('-')
-        folder.invokeFactory('collective.person.person', person)
-        folder[person].given_name = fullname[0].capitalize()
-        folder[person].surname = fullname[1].capitalize()
+        image = os.path.join(os.path.dirname(__file__), 'profiles', 'demo',
+                             'images', 'picture%s.png' % user['number'])
+        data = getFile(image).read()
+        folder.invokeFactory('collective.person.person', person,
+            birthday=datetime.date(datetime(1985, 2, 17)),
+            picture=NamedImage(data),
+            given_name=fullname[0].capitalize(),
+            surname=fullname[1].capitalize(),
+            gender=u'n/a',
+            )
         p1_contact = IContactInfo(folder[person])
+        p1_contact.emails = [{'category': u'work',
+                                          'data': u'%s@simples.com.br' %
+                                         person.replace('-', '.')},
+                             {'category': u'home',
+                                          'data': u'%s@gmail.com' %
+                                         person.replace('-', '.')}]
         p1_contact.instant_messengers = [{'category': u'skype',
-                                          'data': u'%s' % person.replace('-','_')}]
+                                          'data': u'%s' %
+                                         person.replace('-', '_')}]
+        p1_contact.telephones = [{'category': 'home', 'data': '+5511555.1213'},
+                                 {'category': 'work', 'data': '+5511316.9876'}]
         p1_ploneuser = IPloneUser(folder[person])
         p1_ploneuser.user_name = person
         folder[person].reindexObject()
+        review_state = folder[person].portal_workflow.getInfoFor(
+                                                            folder[person],
+                                                            'review_state')
+        if not review_state == 'published':
+            folder[person].portal_workflow.doActionFor(folder[person],
+                                                       'publish')
 
     import transaction
     transaction.commit()
-
-    logger = logging.getLogger(PROJECTNAME)
-    setup.run_upgrades_for_profile(PROFILE_ID, context, logger)
 
 
 def create_user(username, password, portal):
